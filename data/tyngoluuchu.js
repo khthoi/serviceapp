88a70cc4d3f8aa0c106f511f9aced230b7ece3ab
khthoi <pvpgammingpro@gmail.com>
2024-11-24 20:49:38 +0700

    test

---
+++ data/tyngoluuchu.js
@@ -0,0 +1,148 @@
+const data = {
+    "Tý": {
+        "Giáp": "",
+        "Ất": "Tiền Cốc",
+        "Bính": "",
+        "Đinh": "Tam Gian, Uyển Cốt",
+        "Mậu": "",
+        "Kỷ": "Dương Phụ",
+        "Canh": "",
+        "Tân": "Túc Tam Lý",
+        "Nhâm": "",
+        "Quý": "Nạp Quan Xung"
+    },
+    "Sửu": {
+        "Giáp": "Hành Gian",
+        "Ất": "",
+        "Bính": "Thái Bạch, Thái Xung",
+        "Đinh": "",
+        "Mậu": "Phục Lưu",
+        "Kỷ": "",
+        "Canh": "Thiếu Hải",
+        "Tân": "",
+        "Nhâm": "Nạp Khúc Trạch",
+        "Quý": ""
+    },
+    "Dần": {
+        "Giáp": "",
+        "Ất": "Khâu Khư, Hãm Cốc",
+        "Bính": "",
+        "Đinh": "Côn Lôn",
+        "Mậu": "",
+        "Kỷ": "Tiểu Hải",
+        "Canh": "",
+        "Tân": "Nạp Thiên Tinh",
+        "Nhâm": "Chí Âm",
+        "Quý": ""
+    },
+    "Mão": {
+        "Giáp": "Thần Môn, Thái Khê, Đại Lăng",
+        "Ất": "",
+        "Bính": "Kính Cừ",
+        "Đinh": "",
+        "Mậu": "Khúc Tuyền",
+        "Kỷ": "",
+        "Canh": "Nạp Gian Sư",
+        "Tân": "Thiếu Thương",
+        "Nhâm": "",
+        "Quý": ""
+    },
+    "Thìn": {
+        "Giáp": "",
+        "Ất": "Dương Khê",
+        "Bính": "",
+        "Đinh": "Dương Lăng Tuyền",
+        "Mậu": "Nạp Chi Câu",
+        "Kỷ": "Thương Dương",
+        "Canh": "",
+        "Tân": "Hiệp Khê",
+        "Nhâm": "",
+        "Quý": ""
+    },
+    "Tỵ": {
+        "Giáp": "Thương Khâu",
+        "Ất": "",
+        "Bính": "Âm Cốc",
+        "Đinh": "",
+        "Mậu": "Nạp Đại Lăng",
+        "Kỷ": "Ẩn Bạch",
+        "Canh": "",
+        "Tân": "Nhiên Cốc",
+        "Nhâm": "",
+        "Quý": ""
+    },
+    "Ngọ": {
+        "Giáp": "",
+        "Ất": "Ủy Trung",
+        "Bính": "",
+        "Đinh": "Nạp Trung Chử",
+        "Mậu": "Lệ Đoài",
+        "Kỷ": "",
+        "Canh": "Thông Cốc",
+        "Tân": "",
+        "Nhâm": "Hậu Khê, Kinh Cốt, Dương Trì",
+        "Quý": ""
+    },
+    "Mùi": {
+        "Giáp": "Xích Trạch",
+        "Ất": "",
+        "Bính": "Nạp Lao Cung",
+        "Đinh": "Thiếu Xung",
+        "Mậu": "",
+        "Kỷ": "Ngư Tế",
+        "Canh": "",
+        "Tân": "Thái Xung, Thái Uyên",
+        "Nhâm": "",
+        "Quý": ""
+    },
+    "Thân": {
+        "Giáp": "",
+        "Ất": "Nạp Dịch Môn",
+        "Bính": "Thiếu Trạch",
+        "Đinh": "",
+        "Mậu": "Nhị Gian",
+        "Kỷ": "",
+        "Canh": "Lâm Khấp, Hợp Cốc",
+        "Tân": "",
+        "Nhâm": "Giải Khê",
+        "Quý": ""
+    },
+    "Dậu": {
+        "Giáp": "Nạp Trung Xung",
+        "Ất": "Đại Đôn",
+        "Bính": "",
+        "Đinh": "Thái Khê, Thái Bạch",
+        "Mậu": "",
+        "Kỷ": "Linh Đạo",
+        "Canh": "",
+        "Tân": "",
+        "Nhâm": "",
+        "Quý": ""
+    },
+    "Tuất": {
+        "Giáp": "Khiếu Âm",
+        "Ất": "",
+        "Bính": "Nội Đình",
+        "Đinh": "",
+        "Mậu": "Thúc Cốt, Xung Dương",
+        "Kỷ": "",
+        "Canh": "Dương Cốc",
+        "Tân": "",
+        "Nhâm": "Khúc Trì",
+        "Quý": ""
+    },
+    "Hợi": {
+        "Giáp": "",
+        "Ất": "Thiếu Phủ",
+        "Bính": "",
+        "Đinh": "Thái Uyên, Thần Môn",
+        "Mậu": "",
+        "Kỷ": "Trung Phong",
+        "Canh": "",
+        "Tân": "Âm Lăng Tuyền",
+        "Nhâm": "",
+        "Quý": "Dũng Tuyền"
+    }
+}
+
+export { data };
